49d3b12b9fb248808a97197470cb9d17ffa1055c
Zakrajsk <gal.zakrajsek@gmail.com>
2022-03-23 23:53:13 +0100

    Intersect fix and first LR working

--- pyvor/delaunay_triangulation.py
+++ pyvor/delaunay_triangulation.py
@@ -1,5 +1,5 @@
-from cgi import test
-from sys import api_version
+from cgitb import small
+from operator import le
 from point import Point
 from triangle import Triangle
 from ploting import PlotPoints, PlotTriangles, PlotConnectedPoints
@@ -13,6 +13,26 @@ def OrderPoints(points):
     #TODO Implement it
     raise NotImplementedError("Not yet implemented")
 
+
+def AngleBetweenLines(point1, point2, point3, point4):
+    """
+    Calculates angle between line from point1 to point2 and point3 to point4
+    """
+    x1, y1 = point1.GetCoords()
+    x2, y2 = point2.GetCoords()
+    x3, y3 = point3.GetCoords()
+    x4, y4 = point4.GetCoords()
+
+    vector1 = (x2 - x1, y2 - y1)
+    vector2 = (x4 - x3, y4 - y3)
+
+    vector1_x_vector2 = vector1[0] * vector2[0] + vector1[1] * vector2[1]
+    vector1_norm = math.sqrt(vector1[0] ** 2 + vector1[1] ** 2)
+    vector2_norm = math.sqrt(vector2[0] ** 2 + vector2[1] ** 2)
+
+    return math.degrees(math.acos(vector1_x_vector2 / (vector1_norm * vector2_norm)))
+
+
 def CheckIfIntersects(point1, point2, point3, point4):
     """
     Check if line from point1 to point2 intersects with line from point3 to point4
@@ -25,13 +45,35 @@ def CheckIfIntersects(point1, point2, point3, point4):
     #missing check if one line is on another!!!
     d = ((y2 - y1) * (x3 - x4) - (x2 - x1) * (y3 - y4))
     if d == 0:
-        #Intersection doesnt exist
+        #Lines are paralel so only check if one is on another
+        #find smaller one
+        vector1 = (x2 - x1, y2 - y1)
+        vector2 = (x4 - x3, y4 - y3)
+        vector1_norm = math.sqrt(vector1[0] ** 2 + vector1[1] ** 2)
+        vector2_norm = math.sqrt(vector2[0] ** 2 + vector2[1] ** 2)
+
+        #take both point of smaller one and check if at least one is on the line
+        if vector1_norm < vector2_norm:
+            larger_vector = vector2
+            larger_point = point3.GetCoords()
+            points = [point1.GetCoords(), point2.GetCoords()]
+        else:
+            larger_vector = vector1
+            larger_point = point1.GetCoords()
+            points = [point3.GetCoords(), point4.GetCoords()]            
+        
+        #get k and n for larger vector y = kx + n
+        k = larger_vector[1] / larger_vector[0]
+        n = larger_point[1] - (k * larger_point[0])
+        for point in points:
+            if k * point[0] + n == point[1]:
+                return True
         return False
 
     alpha = ((y3 - y4) * (x1 - x3) - (x3 - x4) * (y1 - y3)) / d
-
     beta = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / d
     return 0 < alpha < 1 and 0 < beta < 1
+
 
 def RecursiveDivide(points):
     """
@@ -56,13 +98,54 @@ def RecursiveDivide(points):
         left_cell = points[0:n]
         #RR
         right_cell = points[n:]
-
-        #Recursively divide and connect when only 2 or 3 points
         RecursiveDivide(left_cell)
         RecursiveDivide(right_cell)
+        
+        #and nov ve join both halfes together
 
+        #finding our LR connection
+        for left_lr_point in left_cell:
+            for right_lr_point in right_cell:
+                intersection_found = False
+                #check if our new line intersects with anything other connection
+                for left_conn in left_cell:
+                    for conn in left_conn.connections:
+                        if CheckIfIntersects(left_lr_point, right_lr_point, left_conn, conn):
+                            intersection_found = True
 
+                for right_conn in right_cell:
+                    for conn in right_conn.connections:
+                        if CheckIfIntersects(left_lr_point, right_lr_point, right_conn, conn):
+                            intersection_found = True
 
+                if not intersection_found:
+                    #we found our RL connection
+                    break
+            if not intersection_found:
+                #we break out because found or connection
+                break
+
+        print(left_lr_point, right_lr_point, "TO JE LR")
+        
+        #we nou find left and right candidate for our trianlge (after this step we will complete one triangle)
+        #Our LR vector need to change directions so anlges will be correct!
+        #left candidate
+        all_left_cand = list()
+        for left_candidate in left_lr_point.connections:
+            if not CheckIfIntersects(left_lr_point, right_lr_point, left_lr_point, left_candidate):
+                all_left_cand.append(left_candidate)
+
+        all_right_cand = list()
+        #right candidate
+        for right_candidate in right_lr_point.connections:
+            if not CheckIfIntersects(left_lr_point, right_lr_point, right_lr_point, right_candidate):
+                all_right_cand.append(right_candidate)
+        
+        print(all_left_cand, all_right_cand)
+        
+        
+                
+        
 
 
 
@@ -79,10 +162,11 @@ testne_tocke.append(Point(6, 1))
 testne_tocke.append(Point(6, 2))
 testne_tocke.append(Point(6, 4))
 
+
 #PlotPoints(testne_tocke)
 
-print(CheckIfIntersects(Point(1, 1), Point(1, 4), Point(1, 2), Point(1, 3)))
-
 RecursiveDivide(testne_tocke)
+#for tocka in testne_tocke:
+#   print(tocka.connections)
 
 PlotConnectedPoints(testne_tocke)
